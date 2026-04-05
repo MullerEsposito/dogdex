1545961794b6d83bf26b2b35ac88f4785f9a2e34
mulleresposito <mulleresposito@hotmail.com>
2026-04-04 22:01:30 -0300

    build: configurar app variants (Dev / Prod)
    - Criado app.config.js estendendo as configurações do app.json
    - Configurado APP_ENV no eas.json (development) para alterar o bundle identifier automaticamente e exibir (Dev) no nome do app previnindo sobrescrita ao instalar paralelo ao build Preview/Production.

---
+++ app/app.config.js
@@ -0,0 +1,20 @@
+module.exports = ({ config }) => {
+  const isDev = process.env.APP_ENV === 'development';
+
+  return {
+    ...config,
+    name: isDev ? `${config.name} (Dev)` : config.name,
+    android: {
+      ...config.android,
+      package: isDev 
+        ? `${config.android.package}.dev` 
+        : config.android.package
+    },
+    ios: {
+      ...config.ios,
+      bundleIdentifier: isDev 
+        ? `${config.android.package}.dev` 
+        : config.android.package
+    }
+  };
+};
